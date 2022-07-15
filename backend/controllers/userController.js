356090c0f4411d0f2e9de6ea9d2507be1de7ca65
cdialpha <cdialpha@gmail.com>
2022-07-15 07:09:28 -0400

    changed filename

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler")
 const User = require('../models/User')
-const { genPassword, issueJWT } = require('../lib/passwordUtils');
+const { genPassword, issueJWT, validPassword } = require('../lib/passwordUtils');
 const connectDB = require('../config/db');
 const passport = require('passport'); 
 
@@ -39,7 +39,33 @@ const registerUser = asyncHandler(async (req,res, next) => {
     };
 });
 
+const loginUser = asyncHandler(async (req,res, next) => {
+    await User.findOne({username: req.body.username})
+    .then((user) => {
+
+        if(!user) {
+            res.status(401).json({success: false, msg: "could not find user"});
+        }
+        const isValid = validPassword(req.body.password, user.hash, user.salt) 
+
+        if(isValid) { 
+            const tokenObject = issueJWT(user._id);
+            res.status(200).json({
+                success: true,
+                user,
+                token: tokenObject,
+                expiresIn: tokenObject.expires}) 
+        } else { 
+            res.status(401).json({success: false, msg: "password incorrect"})
+        }
+    })
+    .catch((err)=> {
+        next(err);
+    });
+});
+
 
 module.exports = { 
-    registerUser 
+    registerUser, 
+    loginUser,
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser } = require('../controllers/userController');
+const { registerUser, loginUser } = require('../controllers/userController');
 const passport = require('passport')
 // @desc    Dashboard 
 // @route   POST /register 
@@ -8,63 +8,15 @@ const passport = require('passport')
 
 // @desc     
 // @route   POST /login 
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login-failure', successRedirect: 'login-success'}));
+router.post('/login', loginUser);
 
 
-// When you visit http://localhost:3000/login, you will see "Login Page"
-router.get('/login', (req, res, next) => {
-   
-    const form = '<h1>Login Page</h1><form method="POST" action="/login">\
-    Enter Username:<br><input type="text" name="uname">\
-    <br>Enter Password:<br><input type="password" name="pw">\
-    <br><br><input type="submit" value="Submit"></form>';
-
-    res.send(form);
-
-});
-
-// When you visit http://localhost:3000/register, you will see "Register Page"
-router.get('/register', (req, res, next) => {
-
-    const form = '<h1>Register Page</h1><form method="post" action="register">\
-                    Enter Username:<br><input type="text" name="uname">\
-                    <br>Enter Password:<br><input type="password" name="pw">\
-                    <br><br><input type="submit" value="Submit"></form>';
-
-    res.send(form);
-    
-});
-
-/**
- * Lookup how to authenticate users on routes with Local Strategy
- * Google Search: "How to use Express Passport Local Strategy"
- * 
- * Also, look up what behaviour express session has without a maxage set
- */
-router.get('/protected-route', (req, res, next) => {
-    
-    // This is how you check if a user is authenticated and protect a route.  You could turn this into a custom middleware to make it less redundant
-    if (req.isAuthenticated()) {
-        res.send('<h1>You are authenticated</h1><p><a href="/logout">Logout and reload</a></p>');
-    } else {
-        res.send('<h1>You are not authenticated</h1><p><a href="/login">Login</a></p>');
-    }
-});
-
-// Visiting this route logs the user out
-router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-        if (err) { return next(err);}
-    res.redirect('/protected-route');
-});
-});
-
-router.get('/login-success', (req, res, next) => {
-    res.send('<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>');
-});
-
-router.get('/login-failure', (req, res, next) => {
-    res.send('You entered the wrong password,');
+// @desc     
+// @route   POST /login 
+router.get('/protected', 
+    passport.authenticate('jwt', {session: false}), 
+    (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'you are authorized!'})
 });
 
 module.exports = router;
